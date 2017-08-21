41535f8dbe90288be58003cc394f2665df31f01d
happyendik <vlom4ek@gmail.com>
2017-08-21 09:40:25 +0300

    workaet

--- src/ImageResizeBehavior.php
+++ src/ImageResizeBehavior.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace alexeevdv\image;
+namespace backend\behaviors;
 
 use Yii;
 use yii\base\Behavior;
@@ -15,7 +15,7 @@ use yii\helpers\Url;
 
 /**
  * Class ImageResizeBehavior
- * @package alexeevdv\image
+ * @package backend\behaviors
  */
 class ImageResizeBehavior extends Behavior
 {
@@ -35,7 +35,7 @@ class ImageResizeBehavior extends Behavior
     /**
      * @var null|string
      */
-    public $baseUrl = null;
+    public $basePath = null;
 
     /**
      * @inheritdoc
@@ -49,24 +49,33 @@ class ImageResizeBehavior extends Behavior
     }
 
     /**
+     * @param UploadedFile $file
+     * @return string
+     */
+    private function generateFilename(UploadedFile $file)
+    {
+        return $file->baseName . '.' . $file->extension;
+    }
+
+    /**
      * @param string $attribute
      * @return string
      */
     public function getThumbnail($attribute, $type)
     {
-        if (file_exists(rtrim(Yii::getAlias($this->thumbUploadPath), '/') . '/' . 'thumb-' . $this->owner->$attribute)) {
-            if ($this->baseUrl) {
-                return Url::to(rtrim(Yii::getAlias($this->baseUrl), '/') . '/' . 'thumb-' . $this->owner->$attribute);
-            }
-
-            $filePath =  rtrim($this->thumbUploadPath, '/') . '/' . 'thumb-' . $this->owner->$attribute;
-            $basePath = str_replace('@frontend/web', '', $filePath);
-            return Url::to($basePath);
-        }
-
         $size = $this->thumbnails[$type];
         if (!$size) {
             throw new InvalidParamException('`type` param is missing in model behavior config');
+        }
+
+        if (file_exists(rtrim(Yii::getAlias($this->thumbUploadPath), '/') . '/' . $type . '-' . $this->owner->$attribute)) {
+            if ($this->basePath) {
+                return Url::to(rtrim(Yii::getAlias($this->basePath), '/') . '/' . $type . '-' . $this->owner->$attribute);
+            }
+
+            $fullPath =  rtrim($this->thumbUploadPath, '/') . '/' . $type . '-' . $this->owner->$attribute;
+            $basePath = str_replace('@frontend/web', '', $fullPath);
+            return Url::to($basePath);
         }
 
         $image = Image::getImagine()
@@ -78,27 +87,29 @@ class ImageResizeBehavior extends Behavior
 
         if ($width < $size['width'] || ($height < $size['height'])) {
             if ($size['width'] > $size['height']) {
-                $image->resize(new Box($size['height'] * $ratio, $size['height']));
-            } else {
                 $image->resize(new Box($size['width'], $size['width'] / $ratio));
+            } else {
+                $image->resize(new Box($size['height'] * $ratio, $size['height']));
             }
+            $width = $image->getSize()->getWidth();
+            $height = $image->getSize()->getHeight();
         }
 
-//        $newWidth = $image->getSize()->getWidth();
-//        $newHeight = $image->getSize()->getHeight();
-//        $box = new Box($newWidth, $newHeight);
-//        $center = new Center($box);
+        $box = new Box($width, $height);
+        $center = new Center($box);
+        $centerX = $center->getX() - $size['width'] / 2;
+        $centerY = $center->getY() - $size['height'] / 2;
 
-        $image->crop(new Point(0, 0), new Box($size['width'], $size['height']))
+        $image->crop(new Point($centerX, $centerY), new Box($size['width'], $size['height']))
             ->save(
-                rtrim(Yii::getAlias($this->thumbUploadPath), '/') . '/' . 'thumb-' . $this->owner->$attribute
+                rtrim(Yii::getAlias($this->thumbUploadPath), '/') . '/' . $type . '-' . $this->owner->$attribute
             );
 
-        if ($this->baseUrl) {
-            return Url::to(rtrim(Yii::getAlias($this->baseUrl), '/') . '/' . 'thumb-' . $this->owner->$attribute);
+        if ($this->basePath) {
+            return Url::to(rtrim(Yii::getAlias($this->basePath), '/') . '/' . $type . '-' . $this->owner->$attribute);
         }
 
-        $filePath = rtrim($this->thumbUploadPath, '/') . '/' . 'thumb-' . $this->owner->$attribute;
+        $filePath = rtrim($this->thumbUploadPath, '/') . '/' . $type . '-' . $this->owner->$attribute;
         $basePath = str_replace('@frontend/web', '', $filePath);
         return Url::to($basePath);
     }
